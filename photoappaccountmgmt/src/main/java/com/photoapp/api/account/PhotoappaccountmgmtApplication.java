82aa8f6fa0f79bc9e6983e49456a70b319d00894
neerajpremani <neerajpremani@gmail.com>
2020-06-26 20:28:03 -0400

    Microservice application

---
+++ photoappaccountmgmt/src/main/java/com/photoapp/api/account/PhotoappaccountmgmtApplication.java
@@ -0,0 +1,16 @@
+package com.photoapp.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoappaccountmgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappaccountmgmtApplication.class, args);
+    }
+
+}
---
+++ photoappaccountmgmt/src/test/java/com/photoapp/api/account/PhotoappaccountmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappaccountmgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
